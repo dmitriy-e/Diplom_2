d7ce562c1c4379af78df66ecd14fc752b8051792
Dmitriy <autodim2007@yandex.ru>
2023-03-28 23:58:32 +0300

    Константы и маршруты приложения

---
+++ src/main/java/org/stellar/burgers/api/constants/Constants.java
@@ -0,0 +1,5 @@
+package org.stellar.burgers.api.constants;
+
+public class Constants {
+    public static final String BASE_URI = "https://stellarburgers.nomoreparties.site/api";
+}
---
+++ src/main/java/org/stellar/burgers/api/routes/UserRoutes.java
@@ -0,0 +1,6 @@
+package org.stellar.burgers.api.routes;
+
+public class UserRoutes {
+    public static final String BASIC_ROUTE = "/auth";
+    public static final String REGISTER_ROUTE = BASIC_ROUTE + "/register";
+}
